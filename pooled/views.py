0ce4cb122530959a4dcf62e14455a4f6a0288604
Steve Lounsbury <steve.lounsbury@gmail.com>
2009-03-18 01:37:22 -0400

    Added mockup from mccann, added view skeleton and urls just to test it out

--- pooled/views.py
+++ pooled/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+
+def index(request):
+    return HttpResponse('Pooled.')
